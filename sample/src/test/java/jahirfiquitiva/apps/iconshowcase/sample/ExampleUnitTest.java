0d28f53ff606c9ee09d3a8df2c54fd03417e72c9
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2016-02-08 15:29:01 -0500

    Added Sample back

---
+++ sample/src/test/java/jahirfiquitiva/apps/iconshowcase/sample/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016. Jahir Fiquitiva. Android Developer. All rights reserved.
+ */
+
+package jahirfiquitiva.apps.iconshowcase.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
