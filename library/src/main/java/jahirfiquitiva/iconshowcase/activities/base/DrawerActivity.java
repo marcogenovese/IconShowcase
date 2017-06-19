8fbd64a027e78adb72c5369c245afba7c5d3dd89
Marco Genovese <marcogenovese.dev@gmail.com>
2017-06-19 12:29:24 +0200

    Update DrawerActivity.java

--- library/src/main/java/jahirfiquitiva/iconshowcase/activities/base/DrawerActivity.java
+++ library/src/main/java/jahirfiquitiva/iconshowcase/activities/base/DrawerActivity.java
@@ -82,6 +82,8 @@ public abstract class DrawerActivity extends CapsuleActivity {
                 return DrawerItem.PREVIEWS;
             case "wallpapers":
                 return DrawerItem.WALLPAPERS;
+            case "kustom":
+                return DrawerItem.KUSTOM;
             case "requests":
                 return DrawerItem.REQUESTS;
             case "apply":
@@ -90,8 +92,6 @@ public abstract class DrawerActivity extends CapsuleActivity {
                 return DrawerItem.FAQS;
             case "zooper":
                 return DrawerItem.ZOOPER;
-            case "kustom":
-                return DrawerItem.KUSTOM;
             default:
                 throw new UnsupportedOperationException("Invalid drawer key " + s + ".\nPlease " +
                         "check your primary_drawer_items array");
@@ -137,6 +137,12 @@ public abstract class DrawerActivity extends CapsuleActivity {
                 return new WallpapersFragment();
             }
         },
+        KUSTOM("Kustom", R.string.section_kustom, R.drawable.ic_zooper_kustom) {
+            @Override
+            public Fragment getFragment() {
+                return new KustomFragment();
+            }
+        },
         REQUESTS("Requests", R.string.section_icon_request, R.drawable.ic_request) {
             @Override
             public Fragment getFragment() {
@@ -159,12 +165,6 @@ public abstract class DrawerActivity extends CapsuleActivity {
             @Override
             public Fragment getFragment() {
                 return new ZooperFragment();
-            }
-        },
-        KUSTOM("Kustom", R.string.section_kustom, R.drawable.ic_zooper_kustom) {
-            @Override
-            public Fragment getFragment() {
-                return new KustomFragment();
             }
         },
         CREDITS("Credits", R.string.section_about, 0) {
